4fe6b2390f3b6c35bb49ea689cba46851eb7bad9
Pranay Kumar Jha <125690497+CodeGuy0@users.noreply.github.com>
2025-02-01 01:20:42 +0530

    Updated files with latest

--- app.js
+++ app.js
@@ -16,19 +16,38 @@ const app = express();
 
 
 //app.use callback : random example:
-app.use("/random", (req, res, next) => {
-    console.log("I am only for random");
-    next();
+// app.use("/random", (req, res, next) => {
+//     console.log("I am only for random");
+//     next();
+// });
+
+// ....................................................................................................................
+app.use("/api", (req, res, next) => {
+    let { token } = req.query;
+    if (token === "giveaccess") {
+        next();
+    }
+    res.send("ACCESS DENIED!");
 });
 
+// ACTIVITY :- API TOKEN AS QUERY STRING :- uper jab access milega api ko ussi ke baad data milega warna nhi 
+app.get("/api", (req, res) => {
+    res.send("data");
+});
+//..............................................................................................................................
 
 //logger :- to log information 
 
-app.use((req, res, next) => {
-    req.time = new Date(Date.now()).toString();
-    console.log(req.method, req.hostname, req.path, req.time);
-    next();
-})
+// app.use((req, res, next) => {
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// });
+
+//404
+app.use((req, res) => {
+    res.send("page not found");
+});
 
 app.get("/", (req, res) => {
     res.send("Mai Hu Pranay, THE CODEGUY");
